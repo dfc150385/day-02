94db9f0d8f56dce70ea794eb80b2adec5ddaf63e
duanfanchao <2432352280@qq.com>
2020-06-30 16:27:52 +0800

    提交

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // console.log("1111");
 const http = require("http");
-// const fs = require("fs");
+const fs = require("fs");
 
 // 创建一个web服务器，监听3333端口
 //  req request   请求,客户端向服务器发起
@@ -9,6 +9,27 @@ http
   .createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
-    res.end("hello word");
+    console.log(req.headers);
+    // if (req.url == "/index.html") {
+    //   res.setHeader("Content-Type", "text/html;charset=utf-8"); // 设置请求头
+    //   // res.end("你请求的是index.html");
+    //   const str = fs.readFileSync("./public/index.html").toString();
+    //   res.end(str);
+    // } else {
+    //   res.end("hello word"); // 输出内容
+    // }
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
+    // 创建了一个静态服务器,可以用来解析html等静态文件
+    if (req.url == "/" || req.url == "/favicon.ico") {
+      res.end("hello world");
+    } else {
+      try {
+        const str = fs.readFileSync("./public" + req.url).toString();
+        res.end(str);
+      } catch (err) {
+        res.statusCode = 404; // 设置404状态码
+        res.end("404");
+      }
+    }
   })
   .listen(3333);
